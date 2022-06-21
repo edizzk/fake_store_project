d700c26c968cefc99aea7e3f7b4a55003d3fa7fb
edizzk <edizmetekurt@gmail.com>
2022-06-21 12:40:06 +0300

    add network service

--- lib/features/home/service/home_service.dart
+++ lib/features/home/service/home_service.dart
@@ -1,7 +1,33 @@
+import 'package:fake_store_project/features/home/model/product_model.dart';
 import 'package:vexana/vexana.dart';
+
+import '../../../product/model/query/product_queries.dart';
+
+
+
+
+enum _HomeServicePath { products }
 
 abstract class IHomeService {
   late final INetworkManager _networkManager;
 
-  IHomeService(NetworkManager networkManager) : _networkManager = networkManager;
+  IHomeService(INetworkManager networkManager) : _networkManager = networkManager;
+
+  Future<List<ProductModel>?> fetchAllProducts({int count = 5});
+}
+
+class HomeService extends IHomeService {
+  HomeService(INetworkManager networkManager) : super(networkManager);
+
+
+  @override
+  Future<List<ProductModel>?> fetchAllProducts({int count = 5}) async {
+    final response = await _networkManager.send<ProductModel, List<ProductModel>>(
+        _HomeServicePath.products.name,
+        parseModel: ProductModel(),
+        method: RequestType.GET,
+        queryParameters: Map.fromEntries([ProductQueries.limit.toMapEntry('$count')]));
+
+    return response.data;
+  }
 }
